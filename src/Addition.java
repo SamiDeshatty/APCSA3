a0b1c0ac8ee2e099553bb8d214a9783710158481
SamiDeshatty <sameekshadeshatty@gmail.com>
2022-03-22 11:21:00 -0700

    Initial plus addition

---
+++ src/Addition.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Addition {
+    public Addition() {
+        int one = 4;
+        int two = 71;
+        int seven = 917;
+        int hyunjin = 7592;
+
+        int chan = one + two - seven * hyunjin;
+
+        System.out.println("The value is" + chan);
+    }
+}
