8a5173255896a8754fabac812d7ca9e33c084c48
SamiDeshatty <sameekshadeshatty@gmail.com>
2022-03-29 10:41:36 -0700

    Calculator Update

--- src/Menu.java
+++ src/Menu.java
@@ -30,6 +30,7 @@ public class Menu {
         menu.put(1, new Menu("IntByReference", () -> IntByReference.main(null)));
         menu.put(2, new Menu("Matrix", () -> Matrix.main(null)));
         menu.put(3, new Menu("Queue", () -> QueueTester.main(null)));
+        menu.put(4, new Menu("Calculator", () -> Calculator.main(null)));
 
         //Printing the menu by iterating though HashMap menu
         System.out.println("Please select a program to run:");
